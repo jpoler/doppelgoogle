64892bc18547ee016c40949a5d4188c0da003208
Jon Poler <jdp5087@gmail.com>
2015-01-16 17:48:37 -0500

    Adding a logger that will hopefully work well for concurrent processes

---
+++ logger.py
@@ -0,0 +1,77 @@
+import os
+import sys
+import datetime
+from multiprocessing import Process
+
+if sys.version < 3:
+    range = xrange
+
+
+TEMP_DIR = '/tmp'
+LOG_DIR = os.path.join(TEMP_DIR, "crawl")
+
+def _mkdir(newdir):
+    """works the way a good mkdir should :)
+    - already exists, silently complete
+    - regular file in the way, raise an exception
+    - parent directory(ies) does not exist, make them as well
+    """
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired " \
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            _mkdir(head)
+            #print "_mkdir %s" % repr(newdir)
+        if tail:
+            os.mkdir(newdir)
+
+def prepare_log_dir(dir):
+    if not os.path.exists(dir):
+        _mkdir(dir)
+        return
+    for f in os.listdir(dir):
+        path = os.path.join(dir, f)
+        if os.path.isfile(path):
+            os.remove(path)
+
+class Logger:
+    def __init__(self, dir):
+        self.filepath = os.path.join(dir, str(os.getpid()))
+        if os.path.exists(dir):
+            self.file = open(self.filepath, "w")
+        else:
+            _mkdir(dir)
+            self.file = open(self.filepath, "w")
+
+    def write_log(self, message):
+        with self.file as f:
+            f.write("{}\t{}\t{}".format(
+                datetime.datetime.now().strftime("%H%M%S%f"),
+                os.getpid(),
+                message
+            ))
+
+
+def test_logger():
+    log = Logger(LOG_DIR)
+    for i in range(100):
+        log.write_log("Hooray for concurrency!")
+
+if __name__ == "__main__":
+    prepare_log_dir(LOG_DIR)
+    children = [Process(target=test_logger, args=()) for i in range(10)]
+    for child in children:
+        child.start()
+
+    for i in range(50):
+        print("Parent message")
+
+    for child in children:
+        child.join()
+                   
+        
+    

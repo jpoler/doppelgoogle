0c101e99ef665c4c4456abc54a8619483713380f
Jon Poler <jdp5087@gmail.com>
2015-01-20 16:21:19 -0500

    Created db/insert.py

---
+++ db/insert.py
@@ -0,0 +1,11 @@
+from doppelgoogle.db.models import *
+
+# I am going to attempt to write this top down for one main reason:
+# if I write my functions to be general enough, then I can originally just use
+# direct database queries to see if nodes are already in the database
+# if this method is too slow then I can optimize with in-memory data structures
+# I am going to start with just database queries to keep things simple, and
+# then I'll check performance with cProfile
+
+def insert_data(data):
+    

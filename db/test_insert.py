66f4f031965a1c57d4ee7ef48ee81bb9473198cb
Jon Poler <jdp5087@gmail.com>
2015-01-26 13:53:47 -0500

    Changed name of insert.py to query.py because it makes more sense.

--- db/test_insert.py
+++ db/test_insert.py
@@ -2,7 +2,7 @@ import sys
 import pickle
 from py2neo import Graph
 
-import doppelgoogle.db.insert as insert
+import doppelgoogle.db.query as query
 from doppelgoogle.db.repr import *
 from doppelgoogle.conf.conf import PICKLE_DUMP_PATH
 
@@ -22,7 +22,7 @@ g = Graph()
 tx = g.cypher.begin()
 
 
-uq = insert.UniquenessConstraintQuery(g)
+uq = query.UniquenessConstraintQuery(g)
 constraints = [
     {'label': 'WebPage', 'prop': 'name'},
     {'label': 'Word', 'prop':'name'},
@@ -33,7 +33,7 @@ constraints = [
 
 print(uq.add_constraints_and_execute(constraints))
 
-# iq = insert.IndexQuery(g)
+# iq = query.IndexQuery(g)
 # indexes = [
 #     {'label': 'WebPage', 'prop': 'name'},
 #     {'label': 'Word', 'prop':'name'},
@@ -49,7 +49,7 @@ page_repr1 = WebPageRepr(name='http://en.wikipedia.org/wiki/Computer')
 page_repr2 = WebPageRepr(name='http://www.google.com')
 link_repr1 = LinkRepr(href='http://www.google.com', lang='en')
 
-q = insert.InsertQuery(g)
+q = query.InsertQuery(g)
 q.merge_node(page_repr1)
 q.merge_node(page_repr2)
 q.create_relation(page_repr1, page_repr2, 'LINKS_TO', attrs=link_repr1)
